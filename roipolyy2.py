e791fe9828568f762e020b580a6448d650de5b15
noa-raifler <55491297+noa-raifler@users.noreply.github.com>
2021-11-02 16:48:17 +0200

    more - roipoly - 2.11.21

--- roipolyy2.py
+++ roipolyy2.py
@@ -6,7 +6,8 @@ import logging
 
 
 # Create image
-img = np.ones((100, 100)) * range(0, 100)
+img = cv2.imread('24_14_09_56_065849_101933.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 print(img)
 
 # Show the image
@@ -29,7 +30,7 @@ roi1.display_roi()
 plt.title('draw second ROI')
 plt.show(block=False)
 
-# # Let user draw second ROI
+# Let user draw second ROI
 roi2 = RoiPoly(color='b', fig=fig)
 
  # Show the image with both ROIs and their mean values
@@ -37,30 +38,23 @@ plt.imshow(img, interpolation='nearest', cmap="Greys")
 plt.colorbar()
 for roi in [roi1, roi2]:
     roi.display_roi()
-    roi.display_mean(img)
+    #roi.display_mean(img)
 plt.title('The two ROIs')
 plt.show()
 
-# Show ROI masks
-plt.imshow(roi1.get_mask(img) + roi2.get_mask(img),
-           interpolation='nearest', cmap="Greys")
-plt.title('ROI masks of the two ROIs')
+mask1 = roi1.get_mask(img[:,:,0])
+print(img[:,:,0])
+img[mask1]=0
+plt.imshow(img, interpolation='nearest', cmap="Greys")
+plt.colorbar()
+plt.title("blup")
 plt.show()
 
-mask1 = roi1.get_mask(img)
 
-result = img[mask1]
-cv2.imshow('res', result)
-cv2.waitKey(0)
-
-
-# print('blup')
-# print(mask1)
-# mask2 = roi2.get_mask(img)
-# print('ma')
-# print(mask1)
-# plt.imshow( np.logical_not(mask1) + np.logical_not(mask2)+ img, interpolation='nearest', cmap="Greys") # show the binary signal mask
-
-#plt.show()
+# # Show ROI masks
+# plt.imshow(roi1.get_mask(img) + roi2.get_mask(img),
+#            interpolation='nearest', cmap="Greys")
+# plt.title('ROI masks of the two ROIs')
+# plt.show()
 
 
