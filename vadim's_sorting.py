4955e8d3f0a412b87141aac1e3839b8e6906257b
noa-raifler <55491297+noa-raifler@users.noreply.github.com>
2021-11-28 18:28:41 +0200

    sorting images for vadim- left only to save the pictures - 28.11.21

--- vadim's_sorting.py
+++ vadim's_sorting.py
@@ -31,15 +31,71 @@ def dividie_names_per_ind_cam(files):
         cameras[idi].append(files[i])
 
 #second, from one camera (each),we divide per day:
-def divide_per_day(files_per_cam):
-    pass
+def divide_per_day(files_per_cam, idi):
+    for file in files_per_cam:
+        basename = os.path.basename(file)
+        day = basename[0:2]
+        sortCamAndDay[idi][day].append(file)
+
+#finally, for each day and camera, sort by the time we took the pic.
+def sorting_in_milsec():
+    new_a = []
+    for i in range (len(ids)):
+        for day in lis.keys():
+            new_a.clear()
+            for basename in sortCamAndDay[ids[i]][day]:
+                try:
+                    only_time = re.findall(day+'_(.*)'+'_'+ids[i], basename)[0]
+                    new_a.append(only_time)
+                except:
+                    continue
+
+            sortt = sorted(new_a) # the array for each day (with only times), sorted by time
+            for j in range(len(sortt)):
+                name = day+'_'+sortt[j]+'_'+ids[i]+'.png'
+                path =find_name_in_full_path(name,sortCamAndDay[ids[i]][day])
+                sortt[j]=path
+            sortCamAndDay[ids[i]][day]=sortt
 
 
 
-files=getListOfFiles('C:\\Users\\noaraifler\\נועה - עבודה\\ניסוי במכון הביולוגי\\תמונות מה-USB של הניסוי')
-print(files[0], len (files))
+def find_name_in_full_path(name,paths):
+    for path in paths:
+        if name in path:
+            return path
+
+
+def copy_to_diff_path():
+    for i in range (len(ids)):
+        for day in lis.keys():
+            for path in sortCamAndDay[ids[i]][day]:
+                pic = cv2.imread(path)
+                basename = os.path.basename(path)
+                cv2.imwrite(basename,pic)
+
+
+init_path = 'C:\\Users\\noaraifler\\נועה - עבודה\\ניסוי במכון הביולוגי\\תמונות מה-USB של הניסוי'
+files=getListOfFiles(init_path)
+#print(files[0], len (files))
 ids= ['101933', '101935', '101934', '101936', '192900073']
 #cameras = [i for i in range(5)]
 cameras={'101933':[], '101935':[], '101934':[], '101936':[], '192900073':[]}
-print(cameras)
+sortCamAndDay = {'101933':[], '101935':[], '101934':[], '101936':[], '192900073':[]}
+dividie_names_per_ind_cam(files)
+
+lis = {'20': [], '24':[], '25':[],'26':[]}
+for idi in sortCamAndDay.keys():
+    sortCamAndDay[idi] = lis.copy()
+
+print(sortCamAndDay)
+
+for idi in cameras.keys():
+    divide_per_day(cameras[idi], idi)
+
+print(sortCamAndDay[ids[1]]['26'])
+
+sorting_in_milsec()
+print(sortCamAndDay)
+
+
 
